029d9bc6d4184d78bce5985fbdccf3b2df9c7a93
Louisa SUDRE-ROUFFAUX <lsudre-r@e3r10p19.42.fr>
2019-10-08 12:41:45 +0200

    ft_isalnum.c 	ft_isalpha.c 	ft_isascii.c 	ft_isdigit.c 	ft_isprint.c 	ft_strlcpy.c 	ft_strlen.c 	ft_strncmp.c

---
+++ ft_strncmp.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsudre-r <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/07 15:11:29 by lsudre-r          #+#    #+#             */
+/*   Updated: 2019/10/07 15:11:31 by lsudre-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t i;
+
+	i = 0;
+	if (n <= 0)
+		return (0);
+	while (s1[i] == s2[i] && i < n - 1)
+	{
+		if (s1[i] == '\0')
+			return (0);
+		else
+			i++;
+	}
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}

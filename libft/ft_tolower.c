61356ad9f42a4344e593824f50b3b99aa7b53036
Louisa SUDRE-ROUFFAUX <lsudre-r@e2r2p23.42.fr>
2019-10-09 19:28:24 +0200

    new day e

--- libft/ft_tolower.c
+++
@@ -1,7 +0,0 @@
-
-int ft_tolower(int c)
-{
-    if (c >= 1 && c <= 26)
-        c = c + 32;
-    return (c);
-}
--- libft/ft_toupper.c
+++
@@ -1,7 +0,0 @@
-
-int ft_toupper(int c)
-{
-    if (c >= 33 && c <= 58)
-        c = c - 32;
-    return (c);
-}

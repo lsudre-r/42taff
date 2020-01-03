02543a4f9e35fac168a8e4eb564b636bd960db59
Louisa SUDRE-ROUFFAUX <lsudre-r@e1r12p10.42.fr>
2020-01-03 19:51:25 +0100

    fbwekjf

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -12,28 +12,49 @@
 
 #include "get_next_line.h"
 
-size_t	ft_strlen(const char *str);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
-char	*ft_strdup(const char *s);
-char	*ft_strjoin(char const *s1, char const *s2);
-
-
-static int			gnl_verif_nline(char **stack, char **ligne)
+static	int			gnl_fill_line(char **line, char **stack)
 {
-	char			*tmp_stack;
-	char			*strchr_stack;
-	int				i;
+	int		i;
+	char	*tmp;
 
 	i = 0;
-	strchr_stack = *stack;
-	while (strchr_stack[i] != '\n')
-		if (!strchr_stack[i++])
-			return (0);
-	tmp_stack = &strchr_stack[i];
-	*tmp_stack = '\0';
-	*ligne = ft_strdup(*stack);
-	*stack = ft_strdup(tmp_stack + 1);
-	return (1);
+	while ((*stack)[i] && (*stack)[i] != '\n')
+		++i;
+	if ((*stack)[i] == '\n')
+	{
+		(*stack)[i] = '\0';
+		*line = ft_strdup(*stack);
+		tmp = ft_strdup(*stack + i + 1);
+		free(*stack);
+		*stack = tmp;
+		if (**stack == '\0')
+		{
+			free(*stack);
+			*stack = NULL;	
+		}
+		return (1);
+	}
+	else
+	{
+		if (!(*line = ft_strdup(*stack)))
+			return (-1);
+		free(*stack);
+		*stack = NULL;
+		return (0);
+	}
+}
+static	int			gnl_verif_nline(char *buf)
+{
+	int		i;
+
+	i = 0;
+	while (*(buf + i))
+	{
+		if (*(buf + i) == '\n')
+			return (i);
+		++i;
+	}
+	return (0);
 }
 
 static	int			gnl_read_file(int fd, char *heap, char **stack, char **ligne)
@@ -44,20 +65,21 @@ static	int			gnl_read_file(int fd, char *heap, char **stack, char **ligne)
 	while ((ret = read(fd, heap, BUFFER_SIZE)) > 0)
 	{
 		heap[ret] = '\0';
-		if (*stack)
-		{
-			tmp_stack = *stack;
-			*stack = ft_strjoin(tmp_stack, heap);
-			free(tmp_stack);
-			tmp_stack = NULL;
-		}
-		else
-			*stack = ft_strdup(heap);
-		if (gnl_verif_nline(stack, ligne))
+		tmp_stack = ft_strjoin(*stack, heap);
+		free(*stack);
+		*stack = tmp_stack;
+		if (gnl_verif_nline(*stack))
 			break ;
 	}
-	ret = ret > 0 ? 1 : ret;
-	return (ret);
+	if (ret < 0)
+		return (-1);
+	if (!(*stack) && ret == 0)
+	{
+		if (!(*ligne = ft_strdup("")))
+			return (-1);
+		return (0);
+	}
+	return (gnl_fill_line(ligne, stack));
 }
 
 int					get_next_line(int const fd, char **line)
@@ -67,24 +89,13 @@ int					get_next_line(int const fd, char **line)
 	int				ret;
 	int				i;
 
-	if (!line || (fd < 0 || fd >= MAX_FD) || (read(fd, stack[fd], 0) < 0) \
+	if (!line || (fd < 0 || fd >= MAX_FD) || (read(fd, stack[fd], 0) < 0)
 		|| !(heap = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1)))
 		return (-1);
-	if (stack[fd])
-		if (gnl_verif_nline(&stack[fd], line))
-			return (1);
 	i = 0;
 	while (i < BUFFER_SIZE)
 		heap[i++] = '\0';
-	ret = gnl_read_file(fd, heap, &stack[fd], line);
+	ret = gnl_read_file(fd, heap, stack + fd, line);
 	free(heap);
-	if (ret != 0 || stack[fd] == NULL || stack[fd][0] == '\0')
-	{
-		if (!ret && *line)
-			*line = NULL;
-		return (ret);
-	}
-	*line = stack[fd];
-	stack[fd] = NULL;
-	return (1);
+	return (ret);
 }
